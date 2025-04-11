6f898f73a6151271a74a04890279b981b3f0d137
Shayan Ahmadi <shayanahmadi2004@gmail.com>
2025-04-11 17:33:00 +0330

    fix: prettifier auth controller and gaurd

--- modules/auth/auth.controller.js
+++ modules/auth/auth.controller.js
@@ -1,111 +1,118 @@
-const createHttpError = require("http-errors")
-const { User, Otp } = require("../user/user.model")
-const otpGenerator = require('otp-generator')
-const { config } = require("dotenv")
-const jwt = require("jsonwebtoken")
-const { RefreshToken } = require("./refreshToken.model")
-config()
+const createHttpError = require("http-errors");
+const { User, Otp } = require("../user/user.model");
+const otpGenerator = require("otp-generator");
+const { config } = require("dotenv");
+const jwt = require("jsonwebtoken");
+const { RefreshToken } = require("./refreshToken.model");
+config();
 
 async function sendOtp(req, res, next) {
-    try {
-        const { mobile } = req.body
-        let user = await User.findOne({ where: { mobile } })
-        if (!user) user = await User.create({ mobile })
+  try {
+    const { mobile } = req.body;
+    let user = await User.findOne({ where: { mobile } });
+    if (!user) user = await User.create({ mobile });
 
-        let otp = await Otp.findOne({ where: { userId: user.id } })
+    let otp = await Otp.findOne({ where: { userId: user.id } });
 
-        if (otp) {
-            const now = new Date()
-            const isNotExpired = otp.expires.getTime() > now.getTime()
+    if (otp) {
+      const now = new Date();
+      const isNotExpired = otp.expires.getTime() > now.getTime();
 
-            if (isNotExpired) throw createHttpError(400, "last otp has not expired yet")
-            else {
-                await otp.destroy()
-            }
-        }
-        otp = await Otp.create({
-            userId: user.id,
-            code: otpGenerator.generate(5, { upperCaseAlphabets: false, specialChars: false, lowerCaseAlphabets: false })
-        })
-        return res.json({
-            message: "otp generated successfully",
-            code: otp.code,
-        })
-
-    } catch (error) {
-        next(error)
+      if (isNotExpired)
+        throw createHttpError(400, "last otp has not expired yet");
+      else {
+        await otp.destroy();
+      }
     }
+    otp = await Otp.create({
+      userId: user.id,
+      code: otpGenerator.generate(5, {
+        upperCaseAlphabets: false,
+        specialChars: false,
+        lowerCaseAlphabets: false,
+      }),
+    });
+    return res.json({
+      message: "otp generated successfully",
+      code: otp.code,
+    });
+  } catch (error) {
+    next(error);
+  }
 }
 async function checkOtp(req, res, next) {
-    try {
-        const { mobile, code } = req.body
-        let user = await User.findOne({ where: { mobile } })
-        if (!user) throw createHttpError(401, "not found user account")
-        const otp = await Otp.findOne({ where: { userId: user.id } })
-        if (otp?.code !== code) throw createHttpError(401, "otp code is invalid")
-        if (otp?.expires.getTime() < new Date().getTime()) throw createHttpError(401, "otp is expired")
+  try {
+    const { mobile, code } = req.body;
+    let user = await User.findOne({ where: { mobile } });
+    if (!user) throw createHttpError(401, "not found user account");
+    const otp = await Otp.findOne({ where: { userId: user.id } });
+    if (otp?.code !== code) throw createHttpError(401, "otp code is invalid");
+    if (otp?.expires.getTime() < new Date().getTime())
+      throw createHttpError(401, "otp is expired");
 
-        const { accessToken, refreshToken } = generateToken({ userId: user.id })
+    const { accessToken, refreshToken } = generateToken({ userId: user.id });
 
-        return res.json({
-            message: "logged in successfully",
-            accessToken,
-            refreshToken
-        })
-
-    } catch (error) {
-        next(error)
-    }
+    return res.json({
+      message: "logged in successfully",
+      accessToken,
+      refreshToken,
+    });
+  } catch (error) {
+    next(error);
+  }
 }
 
 async function verifyRefreshToken(req, res, next) {
-    try {
-        const { refreshToken: token } = req.body
-        if (!token) throw createHttpError(401, "enter the refresh token")
+  try {
+    const { refreshToken: token } = req.body;
+    if (!token) throw createHttpError(401, "Enter the refresh token");
 
-        const verified = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET)
+    const verified = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
 
-        if (verified?.userId) {
-            const user = await User.findByPk(verified.userId)
-            if (!user) throw createHttpError(401, "user does not exist")
-            const existToken = await RefreshToken.findOne({
-                where: {
-                    token
-                }
-            })
-            if(existToken) throw createHttpError(401 , "token expired")
-                await RefreshToken.create({
-                    userId: user.id,
-                    token
-                })
-            const { accessToken, refreshToken } = generateToken({ userId: user.id })
+    if (!verified?.userId) throw createHttpError(400, "Invalid refresh token");
 
-            return res.json({
-                message: "token refreshed",
-                accessToken,
-                refreshToken
-            })
-        }
-        return createHttpError(400, "incorrect refresh token")
+    const user = await User.findByPk(verified.userId);
+    if (!user) throw createHttpError(401, "User does not exist");
 
-    } catch (error) {
-        next(createHttpError(401, "login to your account"))
-    }
+    const existToken = await RefreshToken.findOne({ where: { token } });
+    if (!existToken) throw createHttpError(401, "Token expired or invalid");
+
+    await existToken.destroy();
+
+    // تولید توکن جدید
+    const { accessToken, refreshToken: newRefreshToken } = generateToken({
+      userId: user.id,
+    });
+
+    // ذخیره توکن جدید
+    await RefreshToken.create({
+      userId: user.id,
+      token: newRefreshToken,
+    });
+
+    return res.json({
+      message: "Token refreshed",
+      accessToken,
+      refreshToken: newRefreshToken,
+    });
+  } catch (error) {
+    next(createHttpError(401, "Login to your account"));
+  }
 }
-function generateToken(payload) {
-    const { ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } = process.env
-    const accessToken = jwt.sign(payload, ACCESS_TOKEN_SECRET, {
-        expiresIn: "7d"
-    })
-    const refreshToken = jwt.sign(payload, REFRESH_TOKEN_SECRET, {
-        expiresIn: "30d"
-    })
-    return { accessToken, refreshToken }
 
+function generateToken(payload) {
+  const { ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } = process.env;
+  const accessToken = jwt.sign(payload, ACCESS_TOKEN_SECRET, {
+    expiresIn: "7d",
+  });
+  const refreshToken = jwt.sign(payload, REFRESH_TOKEN_SECRET, {
+    expiresIn: "30d",
+  });
+  return { accessToken, refreshToken };
 }
 
 module.exports = {
-    sendOtp,
-    checkOtp,
-    verifyRefreshToken
-}
+  sendOtp,
+  checkOtp,
+  verifyRefreshToken,
+};
--- modules/auth/auth.gaurd.js
+++ modules/auth/auth.gaurd.js
@@ -1,28 +1,26 @@
-const { config } = require('dotenv');
-const { Strategy, ExtractJwt } = require('passport-jwt');
-const { User } = require('../user/user.model');
-const passport = require('passport');
-config()
-var opts = {
-
-}
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+const { config } = require("dotenv");
+const { Strategy, ExtractJwt } = require("passport-jwt");
+const { User } = require("../user/user.model");
+const passport = require("passport");
+config();
 
 async function verifyCallback(payload, done) {
-    try {
-        const user = await User.findByPk(payload.userId);
-        if (user) {
-            return done(null, user);
-        } else {
-            return done(null, false);
-        }
-    } catch (error) {
-        return done(error, false);
+  try {
+    const user = await User.findByPk(payload.userId);
+    if (user) {
+      return done(null, user);
+    } else {
+      return done(null, false);
     }
+  } catch (error) {
+    return done(error, false);
+  }
 }
-const strategy = new Strategy({
+const strategy = new Strategy(
+  {
     secretOrKey: process.env.ACCESS_TOKEN_SECRET,
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
-
-}, verifyCallback)
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  },
+  verifyCallback
+);
 passport.use("jwt", strategy);
